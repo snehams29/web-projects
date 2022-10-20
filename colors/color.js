2bdb9b125152172fd84272564d5b41237001a301
sangamone <98866361+snehams29@users.noreply.github.com>
2022-10-19 22:56:06 -0700

    Add files via upload

---
+++ colors/color.js
@@ -0,0 +1,554 @@
+function fun(){
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun1(){
+    var copyText = document.getElementById("myInput1");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun2(){
+    var copyText = document.getElementById("myInput2");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun3(){
+    var copyText = document.getElementById("myInput3");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun4(){
+    var copyText = document.getElementById("myInput4");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun5(){
+    var copyText = document.getElementById("myInput5");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun6(){
+    var copyText = document.getElementById("myInput6");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun7(){
+    var copyText = document.getElementById("myInput7");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun8(){
+    var copyText = document.getElementById("myInput8");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun9(){
+    var copyText = document.getElementById("myInput9");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun10(){
+    var copyText = document.getElementById("myInput10");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+
+
+function fun11(){
+    var copyText = document.getElementById("myInput11");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun12(){
+    var copyText = document.getElementById("myInput12");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun13(){
+    var copyText = document.getElementById("myInput13");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun14(){
+    var copyText = document.getElementById("myInput14");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun15(){
+    var copyText = document.getElementById("myInput15");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun16(){
+    var copyText = document.getElementById("myInput16");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun17(){
+    var copyText = document.getElementById("myInput17");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun18(){
+    var copyText = document.getElementById("myInput18");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun19(){
+    var copyText = document.getElementById("myInput19");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun20(){
+    var copyText = document.getElementById("myInput20");
+    copyText.select();copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+
+function fun21(){
+    var copyText = document.getElementById("myInput21");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun22(){
+    var copyText = document.getElementById("myInput22");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
+function fun23(){
+    var copyText = document.getElementById("myInput23");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun24(){
+    var copyText = document.getElementById("myInput24");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun25(){
+    var copyText = document.getElementById("myInput25");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun26(){
+    var copyText = document.getElementById("myInput26");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun27(){
+    var copyText = document.getElementById("myInput27");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun28(){
+    var copyText = document.getElementById("myInput28");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun29(){
+    var copyText = document.getElementById("myInput29");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun30(){
+    var copyText = document.getElementById("myInput30");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+
+function fun31(){
+    var copyText = document.getElementById("myInput31");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun32(){
+    var copyText = document.getElementById("myInput32");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+  
+}
+function fun33(){
+    var copyText = document.getElementById("myInput33");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun34(){
+    var copyText = document.getElementById("myInput34");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun35(){
+    var copyText = document.getElementById("myInput35");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun36(){
+    var copyText = document.getElementById("myInput36");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun37(){
+    var copyText = document.getElementById("myInput37");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+
+}
+function fun38(){
+    var copyText = document.getElementById("myInput38");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
+function fun39(){
+    var copyText = document.getElementById("myInput39");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard
+      .writeText(copyText.value)
+      .then(() => {
+        alert("You have copied the colour " + copyText.value +" to your Clipboard");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+ 
+}
